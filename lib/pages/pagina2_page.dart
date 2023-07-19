06245a3dd4d49454da1fc1a0259413d83b398880
Kevin <kamcak@hotmail.es>
2023-07-18 22:11:11 -0500

    S11 - V126 - Diseño Listo

---
+++ lib/pages/pagina2_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Pagina2Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pagina 2'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          MaterialButton(
+            child: Text(
+              'Establecer Usuario',
+              style: TextStyle(color: Colors.white),
+            ),
+            color: Colors.blue,
+            onPressed: () {},
+          ),
+          MaterialButton(
+            child: Text(
+              'Cambiar Edad',
+              style: TextStyle(color: Colors.white),
+            ),
+            color: Colors.blue,
+            onPressed: () {},
+          ),
+          MaterialButton(
+            child: Text(
+              'Añadir Profesión',
+              style: TextStyle(color: Colors.white),
+            ),
+            color: Colors.blue,
+            onPressed: () {},
+          ),
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.accessibility_new),
+        onPressed: () => Navigator.pushNamed(context, 'pagina1'),
+      ),
+    );
+  }
+}
